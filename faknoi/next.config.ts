cd8b89b2cc16219b37b1601c5c4c0f9756181ced
thanadolwi <thanadol.wijarn@gmail.com>
2026-03-15 19:10:19 +0700

    fix: next.config serverExternalPackages

--- faknoi/next.config.ts
+++ faknoi/next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "*.supabase.co" },
     ],
   },
+  serverExternalPackages: ["@supabase/ssr"],
 };
 
 export default nextConfig;
